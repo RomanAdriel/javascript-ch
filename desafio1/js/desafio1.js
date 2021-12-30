e665a2e1d14f0b6e2c607290cb622cd1ddf9e10c
Roman Diaz <roman.diaz09@gmail.com>
2021-12-30 19:34:39 -0300

    limpiar carpetas de desafíos anteriores y agregar estructura de proyecto

--- desafio1/js/desafio1.js
+++
@@ -1,8 +0,0 @@
-let nombre = prompt("Ingrese su nombre: ");
-let apellido = prompt("Ingrese su apellido: ")
-let sueldo = parseInt(prompt("Ingrese su sueldo: "))
-
-let descuentos = sueldo * 0.17
-
-alert("Hola " + nombre + " " + apellido);
-alert("El Estado le quitará de su sueldo la siguiente cantidad: $" + descuentos)
