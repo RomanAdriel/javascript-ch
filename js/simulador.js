eaa2ae679a3bcdde910712d02d178e957816d551
Roman Diaz <roman.diaz09@gmail.com>
2022-01-11 21:24:28 -0300

    Agregar objetos al simulador

--- js/simulador.js
+++ js/simulador.js
@@ -1,3 +1,13 @@
+function Alumno () {
+    this.nombre = "";
+    this.notasTP = 0;
+    this.aplazosTP = 0;
+    this.asistencias = 0;
+    this.notaPrimerPracial = 0;
+    this.notaSegundoParcial = 0;
+    this.notaExamenFinal = 0;
+}
+
 function pedirNombre() {
     let nombreAlumno = prompt("Bienvenido. Ingrese el nombre del alumno: ");
     
@@ -12,9 +22,8 @@ function pedirAsistencias() {
 
 }
 
-function pedirNotasTPs() {
+function pedirNotasTPs(alumno) {
 
-    let aplazos = 0;
     let totalNotas = 0;
     let nota = 0;
 
@@ -22,11 +31,11 @@ function pedirNotasTPs() {
         nota = parseFloat(prompt("Ingrese la nota del TP N°" + i + ":"));
         totalNotas = totalNotas + nota;
         if (nota <= 3) {
-            aplazos++;
+            alumno.aplazosTP++;
         }
     }
 
-    return [totalNotas, aplazos];
+    return totalNotas;
         
 }
   
@@ -63,17 +72,25 @@ function calcularAprobacion(alumno, aplazosTP, promedioTP, notaPrimerPracial, no
 // main
 let cantClases = 25;
 let cantTPs = 5;
-let alumno = pedirNombre();
-let [totalNotasTP, aplazosTP] = pedirNotasTPs();
-let clasesPresente = pedirAsistencias();
-let promedioTP = promedioTPs(totalNotasTP);
+let continuar = "s";
 
-let notaPrimerPracial = parseFloat(prompt("Ingrese la nota del primer parcial: "));
-let notaSegundoParcial = parseFloat(prompt("Ingrese nota del segundo parcial: "));
-let notaExamenFinal = parseFloat(prompt("Ingrese nota del examen final: "));
+while (continuar == "s") {
 
-let porcentajeAsistencias = aprobacionAsistencias(cantClases, clasesPresente);
+    let alumno = new Alumno();
 
-calcularAprobacion(alumno, aplazosTP, promedioTP, notaPrimerPracial, notaSegundoParcial, notaExamenFinal, porcentajeAsistencias);
+    alumno.nombre = pedirNombre();
+    alumno.notasTP = pedirNotasTPs(alumno);
+    alumno.asistencias = pedirAsistencias();
+    let promedioTP = promedioTPs(alumno.notasTP);
 
-  
+    alumno.notaPrimerPracial = parseFloat(prompt("Ingrese la nota del primer parcial: "));
+    alumno.notaSegundoParcial = parseFloat(prompt("Ingrese nota del segundo parcial: "));
+    alumno.notaExamenFinal = parseFloat(prompt("Ingrese nota del examen final: "));
+
+    let porcentajeAsistencias = aprobacionAsistencias(cantClases, alumno.asistencias);
+
+    calcularAprobacion(alumno.nombre, alumno.aplazosTP, promedioTP, alumno.notaPrimerPracial, alumno.notaSegundoParcial, alumno.notaExamenFinal, porcentajeAsistencias);
+
+    continuar = prompt("Desea calcular los datos de otro alumno? (S/N): ").toLowerCase;
+
+}
